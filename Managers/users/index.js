08cb7db0f59872b76f8529ea74b944daf43f8f9e
deouf-dev <113296259+deouf-dev@users.noreply.github.com>
2022-12-27 17:29:40 +0100

    Create index.js

---
+++ Managers/users/index.js
@@ -0,0 +1,33 @@
+class UsersManager extends Map {
+	constructor(userId) {
+		super();
+		this.userId = userId;
+	}
+	getAndCreateIfNotExists() {
+		return this.has(this.userId)
+			? this.get(this.userIdId)
+			: this.set(this.userId, new UserManager(this.userId)).get(this.userId);
+	}
+}
+
+class UserManager extends Map {
+	constructor(userId) {
+		super();
+		this.userId = userId;
+		this.model = require("./model");
+	}
+	async save() {
+		const values = {};
+		this.forEach((v, k) => (values[k] = v));
+		(await this.model.findOne({ userId: this.userId }))
+			? await this.model.updateOne(
+					{ userId: this.userId },
+					{ userId: this.userId, ...values }
+			  )
+			: await this.model.create({ userId: this.userId, ...values });
+
+		return { userId: this.userId, ...values };
+	}
+}
+
+exports.UsersManager = UsersManager;
